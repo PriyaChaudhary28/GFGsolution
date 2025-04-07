7db3d2b3acd72b03056bd5c53ba177f1aa176da4
PriyaChaudhary28 <144237644+PriyaChaudhary28@users.noreply.github.com>
2025-04-08 00:23:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -6,35 +6,39 @@ using namespace std;
 
 // } Driver Code Ends
 
-// User function template for C++
-
 class Solution {
-  
   public:
-    bool isSubsetSum(vector<int>& arr, int target) {
-        int n=arr.size();
-       vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
-      for(int i=0;i<n;i++){
-           dp[i][0]=true;
-      }
+    bool isSubsetSum(vector<int>& arr, int sum) {
         // code here
+        // tabulation 
+ int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(sum + 1, false));
         
-        if (arr[0] <= target) {
-        dp[0][arr[0]] = true;
-    }
-        
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=target;j++){
-               bool nottake=dp[i-1][j];
-               
-               bool take=false;
-               if(arr[i]<=j){
-                   take=dp[i-1][j-arr[i]];
-               }
-               dp[i][j]=nottake || take;
+        // Base case: sum = 0 can always be formed by empty subset
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        // Base case: if first element is within sum
+        if (arr[0] <= sum) {
+            dp[0][arr[0]] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                bool nonpick = dp[i - 1][j];
+                
+                bool pick = false;
+                // 🔥 Fix is here: use j (current target), not sum
+                if (arr[i] <= j) {
+                    pick = dp[i - 1][j - arr[i]];
+                }
+
+                dp[i][j] = pick || nonpick;
             }
         }
-        return dp[n - 1][target];
+
+        return dp[n - 1][sum];
     }
 };
 
